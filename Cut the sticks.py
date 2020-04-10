79d3f7ff91fb72328ee3753cf65ac64dd45a5b6d
God's_father <58758331+God-father1@users.noreply.github.com>
2020-04-10 16:45:20 +0530

    Cut the sticks solution HR

---
+++ Cut the sticks.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+from collections import Counter
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the cutTheSticks function below.
+def cutTheSticks(arr):
+    result = list()
+    l = len(arr)
+    for k,v in sorted(Counter(arr).items()):
+        result.append(l)
+        l-=v
+    return result
+    
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
